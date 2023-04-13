f30823618799ff1cf3987b6b550825a447c3c5c2
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-04-13 11:04:36 +0300

    Update EquipmentDropdown.js

--- client/src/components/Character/EquipmentDropdown.js
+++ client/src/components/Character/EquipmentDropdown.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import EquipmentBox from './EquipmentBox'
-import Character from '../Character';
+import EquipmentBox from './EquipmentBox';
 
 function EquipmentDropdown({ char }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,10 +12,14 @@ function EquipmentDropdown({ char }) {
   function handleItemClick(event) {
     const clickedItem = event.target;
     const tab = clickedItem.getAttribute('value');
-    setSelectedTab(parseInt(tab));
-    setIsOpen(false);
+    if (tab === selectedTab) {
+      setIsOpen(false);
+    } else {
+      setSelectedTab(tab === "Unknown" ? null : parseInt(tab));
+      setIsOpen(false);
+    }
   }
-  
+
   const selectedStats = char.equipment_tabs.find((stats) => stats.tab === selectedTab);
   const activeStats = char.equipment_tabs.find((stats) => stats.is_active);
 
@@ -24,18 +27,17 @@ function EquipmentDropdown({ char }) {
     <>
       <div className="dropdown">
         <button className="dropdown-button" onClick={toggleMenu}>
-          {selectedStats ? selectedStats.name : activeStats.name}
+          {selectedTab === null ? 'Unknown' : (selectedStats ? selectedStats.name : activeStats.name)}
         </button>
         {isOpen && (
           <ul className="dropdown-menu">
             {char.equipment_tabs.map((stats) => (
-              <li key={stats.tab} onClick={handleItemClick} value={stats.tab}>{stats.name}</li>
-            ))};
+              <li key={stats.tab} onClick={handleItemClick} value={stats.name ? stats.tab : 'Unknown'}>{stats.name ? stats.name : 'Unknown'}</li>
+            ))}
           </ul>
         )}
       </div>
       {<EquipmentBox char={char} tab={selectedTab} key={selectedTab} />}
-
     </>
   );
 }
