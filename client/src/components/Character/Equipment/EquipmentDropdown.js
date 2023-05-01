3335395c120ce13af39c0447818eac868550ec6a
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-05-01 13:56:56 +0300

    Worked on the optimization. Migrating the logic from ItemBox to EquipmentDropdown.

--- client/src/components/Character/Equipment/EquipmentDropdown.js
+++ client/src/components/Character/Equipment/EquipmentDropdown.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Equipment from './Equipment';
+import fetchData from '../../fetchData';
 
 function EquipmentDropdown({ char }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,6 +8,7 @@ function EquipmentDropdown({ char }) {
     char.equipment_tabs.find((equip) => equip.is_active)
   );
   const [mergedItems, setMergedItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   function toggleMenu() {
     setIsOpen(!isOpen);
@@ -35,7 +37,49 @@ function EquipmentDropdown({ char }) {
         }
       }
     }
-    setMergedItems(mergedTabs.filter(equip => equip.tabs.includes(selectedTab.tab)));
+    const mergedItems = mergedTabs.filter(equip => equip.tabs.includes(selectedTab.tab));
+
+    const equipItemWithDetails = async (equipItem) => {
+      const itemData = await fetchData('items', equipItem.id);
+      let skinData = {};
+
+      if (equipItem.skin) {
+        skinData = await fetchData('skins', equipItem.skin)
+      }
+
+      const itemDetails = itemData ? itemData.details : null;
+      const stats = itemDetails?.infix_upgrade;
+      const attributesArray = itemDetails?.infix_upgrade?.attributes;
+
+      if (stats && attributesArray) {
+        stats.attributes = {};
+        for (const attributeObj of attributesArray) {
+          stats.attributes[attributeObj.attribute] = attributeObj.modifier;
+        }
+      }
+      return {
+        ...equipItem,
+        rarity: itemData?.rarity,
+        item_name: itemData?.name,
+        item_icon: itemData?.icon,
+        skin_name: skinData?.name,
+        skin_icon: skinData?.icon,
+        stats: equipItem.stats || stats,
+        details: itemDetails,
+        item_data: itemData
+      };
+    };
+
+    const setItemsWithDetails = async (items) => {
+      const newItems = [];
+      for (const item of items) {
+        newItems.push(await equipItemWithDetails(item));
+      }
+      setMergedItems(newItems);
+      setLoading(false);
+    }
+
+    setItemsWithDetails(mergedItems);
   }, [char, selectedTab.tab]);
 
   return (
@@ -57,8 +101,10 @@ function EquipmentDropdown({ char }) {
             ))}
           </ul>
         )}
-      </div>
-      <Equipment key={selectedTab.tab} items={mergedItems} />
+      </div>{loading
+        ? <div>Loading...</div>
+        : <Equipment key={selectedTab.tab} items={mergedItems} />
+      }
     </div>
   );
 }
--- client/src/components/Character/Equipment/ItemBox.js
+++ client/src/components/Character/Equipment/ItemBox.js
@@ -1,107 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Popup from 'reactjs-popup';
-import fetchData from '../../fetchData';
 
 function ItemBox({ item }) {
-    const [fetchedItemId, setFetchedItemId] = useState(null);
-    const [skin, setSkin] = useState(null);
-    const [stats, setStats] = useState(null);
-    const [details, setDetails] = useState(null);
-    const [rarity, setRarity] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        if (!item || !item.id) {
-            return;
-        }
-
-        (async () => {
-            const fetchedItemId = await fetchData('items', item.id);
-            setFetchedItemId(fetchedItemId);
-
-            if (fetchedItemId) {
-                setRarity(fetchedItemId.rarity.toLowerCase());
-            }
-
-            if (item.skin) {
-                const skinResult = await fetchData('skins', item.skin);
-                setSkin(skinResult);
-            }
-
-            if (item.stats) {
-                setStats(item.stats);
-            } else if (fetchedItemId.details.infix_upgrade) {
-                const attributes = fetchedItemId.details.infix_upgrade.attributes;
-                const obj = {};
-                for (const attribute of attributes) {
-                    obj[attribute.attribute] = attribute.modifier;
-                }
-                const result = { id: fetchedItemId.id, attributes: obj };
-                setStats(result);
-            } 
-
-            if (fetchedItemId && fetchedItemId.details) {
-                setDetails(fetchedItemId.details);
-            }
-
-            setLoading(false);
-        })();
-    }, [item]);
-
-    if (loading) {
-        return <img className="item-box box-gray" alt="" />;
-    }
+    console.log('item ', item)
 
     return (
         <>
-            <div>
-                <Popup
-                    trigger=
-                    {skin
-                        ? <img className={`item-box box-${rarity}`} src={skin.icon} alt={skin.id} />
-                        : fetchedItemId
-                            ? <img className={`item-box box-${rarity}`} src={fetchedItemId.icon} alt={fetchedItemId.id} />
-                            : <img className="item-box box-gray" alt="" />
-                    }
-
-                    arrow={false}
-                    position="right center"
-                    on="hover"
-                    offsetX={10}
-                    mouseLeaveDelay={0}
-                >
-                    <Container className='item-popup'>
-                        <Row className={`name-${rarity}`}>
-                            {skin
-                                ? <span>{skin?.name}</span>
-                                : <span>{fetchedItemId?.name}</span>
-                            }
-                        </Row>
-
-                        <br />
-
-                        {details && details.defense !== 0 && details.defense &&
-                            <>
-                                <Row key={`defense${details.defense}`}> Defense: <span className='green'>{details.defense}</span></Row>
-                                <br />
-                            </>
+            {item
+                ? <div>
+                    <Popup
+                        trigger={
+                            item.skin_icon && item.skin_name
+                                ? <img className={`item-box box-${item.rarity.toLowerCase()}`} src={item.skin_icon} alt={item.skin_icon} />
+                                : <img className={`item-box box-${item.rarity.toLowerCase()}`} src={item.item_icon} alt={item.item_icon} />
                         }
-                        {details && details.min_power && details.max_power &&
-                            <>
-                                <Row key={`power${details.min_power}${details.max_power}`}> Weapon Strength: <span className='green'>{details.min_power} - {details.max_power}</span></Row>
-                                <br />
-                            </>
-                        }
-
-                        {stats && stats.attributes && Object.keys(stats.attributes).map(key => (
-                            <Row key={`stats${fetchedItemId.id}${key}`}>
-                                <span className='green'>+ {stats.attributes[key]} {key}</span>
+                        arrow={false}
+                        position="right center"
+                        on="hover"
+                        offsetX={10}
+                        mouseLeaveDelay={0}
+                    >
+                        <Container className='item-popup'>
+                            <Row className={`name-${item.rarity.toLowerCase()}`}>
+                                {item.skin_name
+                                    ? <span>{item.skin_name}</span>
+                                    : <span>{item.item_name}</span>
+                                }
                             </Row>
-                        ))}
-                    </Container>
-                </Popup>
-            </div >
+                            <br />
+                            {item.details.defense !== 0 && item.details.defense &&
+                                <>
+                                    <Row key={`defense${item.details.defense}`}> Defense: <span className='green'>{item.details.defense}</span></Row>
+                                    <br />
+                                </>
+                            }
+                            {item.details.min_power && item.details.max_power &&
+                                <>
+                                    <Row key={`power-${item.details.min_power}${item.details.max_power}`}> Weapon Strength: <span className='green'>{item.details.min_power} - {item.details.max_power}</span></Row>
+                                    <br />
+                                </>
+                            }
+
+                            {item.stats && Object.keys(item.stats.attributes).map(key => (
+                                <Row key={`stats-${item.id}${key}`}>
+                                    <span className='green'>+ {item.stats.attributes[key]} {key}</span>
+                                </Row>
+                            ))}
+                            <br />
+                            {item.skin_name &&
+                                <>
+                                    <div>Transmuted</div>
+                                    <div>{item.skin_name}</div>
+                                </>
+                            }
+                            <br />
+                            <div className={`name-${item.rarity.toLowerCase()}`}>{item.rarity}</div>
+                        </Container>
+                    </Popup>
+                </div >
+                : <img className="item-box box-gray" alt="" />
+            }
         </>
     );
 }
