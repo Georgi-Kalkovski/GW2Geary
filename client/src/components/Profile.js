044710ae14ceb2c77260ea3fcf1322d84bd8cfed
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-06-04 23:08:25 +0300

    Worked on visuals.

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,20 +1,53 @@
+import React, { useState } from "react";
 import AuthService from "../services/auth.service";
 import ProfileInfo from "./Profile/ProfileInfo";
 import ProfileApis from "./Profile/ProfileApis";
 
 const Profile = () => {
   const currentUser = AuthService.getCurrentUser();
+  const [showProfileInfo, setShowProfileInfo] = useState(false);
+  const [showProfileApis, setShowProfileApis] = useState(true);
+
+  const toggleProfileInfo = () => {
+    if (!showProfileInfo) {
+      setShowProfileInfo(true);
+      setShowProfileApis(false);
+    }
+  };
+
+  const toggleProfileApis = () => {
+    if (!showProfileApis) {
+      setShowProfileApis(true);
+      setShowProfileInfo(false);
+    }
+  };
 
   return (
-    <div>
+    <>
       {currentUser && (
-        <div>
-          <ProfileInfo currentUser={currentUser} AuthService={AuthService} />
-          <ProfileApis currentUser={currentUser} AuthService={AuthService} />
-        </div>
-      )
-      }
-    </div >
+        <>
+          <div className="flex center">
+            <button className={`basic-button ${showProfileInfo ? 'active' : ''}`} onClick={toggleProfileInfo}>Profile Info</button>
+            <button className={`basic-button ${showProfileApis ? 'active' : ''}`} onClick={toggleProfileApis}>Profile Apis</button>
+          </div>
+          <div>
+            {showProfileInfo && (
+              <ProfileInfo
+                currentUser={currentUser}
+                AuthService={AuthService}
+              />
+            )}
+
+            {showProfileApis && (
+              <ProfileApis
+                currentUser={currentUser}
+                AuthService={AuthService}
+              />
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
