76b193e59897c578b0747bdf17beac6634a103e4
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-06-05 17:47:20 +0300

    Fixed a bug.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -145,7 +145,7 @@ function Search() {
             <Row>A place where you can inspect equipment and builds of registered accounts and their characters.</Row>
             <Row>
               If someone has registered a valid{' '}
-              <Link to="https://account.arena.net/applications" style={{ color: '#aa0404' }}>
+              <Link to="https://account.arena.net/applications" style={{ color: '#aa0404' }} target="_blank">
                 GW2 API key
               </Link>{' '}
               with us and granted access, you'll be able to find and inspect them.
