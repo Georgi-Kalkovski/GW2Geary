6f3d3db408e09ad5cf7829bedeaf81d2b56e6823
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-06-28 19:17:05 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const User = require('./models/user.model');
 
 const app = express();
 const port = process.env.PORT || 3001;
-const host = 'localhost';
 
 // Enable CORS
 app.use(cors());
@@ -68,7 +67,7 @@ app.post("/reset-password", async (req, res) => {
       subject: "GW2Geary Password Reset",
       html: `<h4>Hi ${user.username},</h4>
              <p>You have requested to reset your password.</p>
-             <p style="display: inline;">Click <h4 style="display: inline;"><a href="http://${host}:3000/reset-password/${token}">here</a></h4> to reset your password.</p>
+             <p style="display: inline;">Click <h4 style="display: inline;"><a href="https://gw2geary.com/reset-password/${token}">here</a></h4> to reset your password.</p>
              <p>If you did not request this, please ignore this email.</p>
              <h4>GW2Geary team.</h4>`,
     };
