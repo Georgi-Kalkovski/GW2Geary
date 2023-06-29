aba041b1e44030135ab3f90a16c1ec3c19383c4a
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-06-29 10:30:10 +0300

    Update ga.js

--- client/public/ga.js
+++ client/public/ga.js
@@ -1,9 +1,13 @@
-// ga.js
-(function (window, document, scriptSrc, gaTrackingID) {
+export function loadGoogleTagManager() {
+    var script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GA_TRACKING_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+  }
+  
+  export function initializeGoogleTag() {
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-  
-    gtag('config', gaTrackingID);
-  })(window, document, 'https://www.googletagmanager.com/gtag/js', `${process.env.REACT_APP_GA_TRACKING_ID}`);
-  
+    gtag('config', `${process.env.REACT_APP_GA_TRACKING_ID}`);
+  }
