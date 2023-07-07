179939816a136bddcaba9c3ca13e270d7efbb42e
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-07-07 21:38:45 +0300

    Update account.routes.js

--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -5,6 +5,9 @@ const { apiKey, baseUrl } = require('./config.routes');
 
 router.get('/', async (req, res) => {
     try {
+        const apiKey = req.headers['x-api-key']; // Retrieve the API key from request headers
+        // Use the API key for further processing
+
         const response = await axios.get(`${baseUrl}/account?${apiKey}`);
         res.json(response.data);
     } catch (error) {
@@ -14,6 +17,9 @@ router.get('/', async (req, res) => {
 
 router.get('/mastery/points', async (req, res) => {
     try {
+        const apiKey = req.headers['x-api-key']; // Retrieve the API key from request headers
+        // Use the API key for further processing
+
         const response = await axios.get(`${baseUrl}/account/mastery/points?${apiKey}`);
         if (response) {
             res.json(response.data);
