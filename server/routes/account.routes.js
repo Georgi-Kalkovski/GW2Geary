a02152f603b1d7d96af998f5e6cef3fa5eb2ffca
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-07-07 22:20:54 +0300

    Update account.routes.js

--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -4,20 +4,22 @@ const router = express.Router();
 const { apiKey, baseUrl } = require('./config.routes');
 
 router.get('/', async (req, res) => {
+    const { access_token } = req.query;
     try {
-        const response = await axios.get(`${baseUrl}/account?${apiKey}`);
-        res.json(response.data);
+        const response = await axios.get(`https://api.guildwars2.com/v2/account?access_token=${access_token}&v=latest`);
+        const accountData = response.data;
+        res.json(accountData);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from API' });
     }
 });
 
 router.get('/mastery/points', async (req, res) => {
+    const { access_token } = req.query;
     try {
-        const response = await axios.get(`${baseUrl}/account/mastery/points?${apiKey}`);
-        if (response) {
-            res.json(response.data);
-        }
+        const response = await axios.get(`https://api.guildwars2.com/v2/account/mastery/points?access_token=${access_token}`);
+        const masteryPoints = response.data;
+        res.json(masteryPoints);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from API' });
     }
