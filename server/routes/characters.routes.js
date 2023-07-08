40a7d6e07fcda4463565e7c1bb1636a09f81374e
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-07-08 03:48:09 +0300

    Update characters.routes.js

--- server/routes/characters.routes.js
+++ server/routes/characters.routes.js
@@ -6,7 +6,6 @@ const User = require('../models/user.model');
 
 router.get('/:name', async (req, res) => {
   const { name } = req.params;
-  console.log(name)
   const user = await User.find(
     { 'apiKeys.characters.name': name },
     { 'apiKeys.$': 1 }
@@ -14,7 +13,6 @@ router.get('/:name', async (req, res) => {
   if (user && user.length > 0) {
     const apiKey = user[0].apiKeys[0];
     const apiKeyId = apiKey._id;
-    console.log(apiKey)
     try {
       const response = await axios.get(`${baseUrl}/characters/${name.replace('_', '%20')}?access_token=${apiKeyId}&v=latest`);
       res.json(response.data);
