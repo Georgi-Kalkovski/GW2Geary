27b30b7d3b22582c1bce2b9891ac55ce6d633be3
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-09-29 18:18:29 +0300

    Did the classes alphabetical.

--- client/src/components/Search/SearchMenu.js
+++ client/src/components/Search/SearchMenu.js
@@ -34,26 +34,26 @@ function SearchMenu({ isOpen, selectedGenderUp, selectedRaceUp, selectedProfessi
                 {isOpen && (
                     <div className='hr-container' style={{ marginTop: '-10px' }}>
                         <div className='flex cursor search-menu-flex'>
-                            <SearchSubbutton active={selectedGender} text='Male' onClick={() => handleGenderSelection('Male')} />
                             <SearchSubbutton active={selectedGender} text='Female' onClick={() => handleGenderSelection('Female')} />
+                            <SearchSubbutton active={selectedGender} text='Male' onClick={() => handleGenderSelection('Male')} />
                             <span className="thin-hr-vertical"></span>
                             <SearchSubbutton active={selectedRace} text='Asura' onClick={() => handleRaceSelection('Asura')} />
-                            <SearchSubbutton active={selectedRace} text='Sylvari' onClick={() => handleRaceSelection('Sylvari')} />
+                            <SearchSubbutton active={selectedRace} text='Charr' onClick={() => handleRaceSelection('Charr')} />
                             <SearchSubbutton active={selectedRace} text='Human' onClick={() => handleRaceSelection('Human')} />
                             <SearchSubbutton active={selectedRace} text='Norn' onClick={() => handleRaceSelection('Norn')} />
-                            <SearchSubbutton active={selectedRace} text='Charr' onClick={() => handleRaceSelection('Charr')} />
+                            <SearchSubbutton active={selectedRace} text='Sylvari' onClick={() => handleRaceSelection('Sylvari')} />
                         </div>
                         <div className="thin-hr"></div>
                         <div className='flex cursor search-menu-flex'>
-                            <SearchSubbutton active={selectedProfession} text='Mesmer' onClick={() => handleProfessionSelection('Mesmer')} />
+                            <SearchSubbutton active={selectedProfession} text='Elementalist' onClick={() => handleProfessionSelection('Elementalist')} />
+                            <SearchSubbutton active={selectedProfession} text='Engineer' onClick={() => handleProfessionSelection('Engineer')} />
                             <SearchSubbutton active={selectedProfession} text='Guardian' onClick={() => handleProfessionSelection('Guardian')} />
+                            <SearchSubbutton active={selectedProfession} text='Mesmer' onClick={() => handleProfessionSelection('Mesmer')} />
                             <SearchSubbutton active={selectedProfession} text='Necromancer' onClick={() => handleProfessionSelection('Necromancer')} />
                             <SearchSubbutton active={selectedProfession} text='Ranger' onClick={() => handleProfessionSelection('Ranger')} />
-                            <SearchSubbutton active={selectedProfession} text='Elementalist' onClick={() => handleProfessionSelection('Elementalist')} />
-                            <SearchSubbutton active={selectedProfession} text='Warrior' onClick={() => handleProfessionSelection('Warrior')} />
-                            <SearchSubbutton active={selectedProfession} text='Thief' onClick={() => handleProfessionSelection('Thief')} />
-                            <SearchSubbutton active={selectedProfession} text='Engineer' onClick={() => handleProfessionSelection('Engineer')} />
                             <SearchSubbutton active={selectedProfession} text='Revenant' onClick={() => handleProfessionSelection('Revenant')} />
+                            <SearchSubbutton active={selectedProfession} text='Thief' onClick={() => handleProfessionSelection('Thief')} />
+                            <SearchSubbutton active={selectedProfession} text='Warrior' onClick={() => handleProfessionSelection('Warrior')} />
                         </div>
                     </div>
                 )}
