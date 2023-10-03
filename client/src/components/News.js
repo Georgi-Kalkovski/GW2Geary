61d15c7eeb36b6869d15d2ee5d614168b56dc8af
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-10-03 14:13:58 +0300

    Update News.js

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import NewsList from './NewsList';
 
 function News() {
     return (
-        <HelmetProvider>
-            <div>
-                <Helmet>
-                    <title>GW2Geary - News</title>
-                    <meta property="og:title" content="GW2Geary - News" />
-                    <meta
-                        name="og:description"
-                        content="All the News about GW2Geary are here! Latest news: Added Relic and Power Core to the character's preview."
-                    />
-                </Helmet>
-                <div className='flex center column'>
-                    <h2 style={{ textAlign: 'center' }}>News</h2>
-                    <div className='about-box news-box' style={{ textAlign: 'left', alignItems: 'right', marginBottom: '20px' }}>
-                        <NewsList />
-                    </div>
+        <div>
+            <Helmet>
+                <title>GW2Geary - News</title>
+                <meta property="og:title" content="GW2Geary - News" />
+                <meta
+                    name="og:description"
+                    content="All the News about GW2Geary are here! 
+                             Latest news: Added Relic and Power Core to the character's preview."
+                />
+            </Helmet>
+            <div className='flex center column'>
+                <h2 style={{ textAlign: 'center' }}>News</h2>
+                <div className='about-box news-box' style={{ textAlign: 'left', alignItems: 'right', marginBottom: '20px' }}>
+                    <NewsList />
                 </div>
             </div>
-        </HelmetProvider>
+        </div>
+
     );
 }
 
