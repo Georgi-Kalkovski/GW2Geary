baa2c696d6d3810f82ce15e431ba1b0c3adfb7b2
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-10-03 14:06:14 +0300

    Working on the meta description.

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import NewsList from './NewsList';
 
 function News() {
     return (
-        <div>
-            <Helmet>
-                <title>GW2Geary - News</title>
-                <meta property="og:title" content="GW2Geary - News" />
-                <meta
-                    name="og:description"
-                    content="All the News about GW2Geary are here! 
-                             Latest news: Added Relic and Power Core to the character's preview."
-                />
-            </Helmet>
-            <div className='flex center column'>
-                <h2 style={{ textAlign: 'center' }}>News</h2>
-                <div className='about-box news-box' style={{ textAlign: 'left', alignItems: 'right', marginBottom: '20px' }}>
-                    <NewsList />
+        <HelmetProvider>
+            <div>
+                <Helmet>
+                    <title>GW2Geary - News</title>
+                    <meta property="og:title" content="GW2Geary - News" />
+                    <meta
+                        name="og:description"
+                        content="All the News about GW2Geary are here! Latest news: Added Relic and Power Core to the character's preview."
+                    />
+                </Helmet>
+                <div className='flex center column'>
+                    <h2 style={{ textAlign: 'center' }}>News</h2>
+                    <div className='about-box news-box' style={{ textAlign: 'left', alignItems: 'right', marginBottom: '20px' }}>
+                        <NewsList />
+                    </div>
                 </div>
             </div>
-        </div>
-
+        </HelmetProvider>
     );
 }
 
