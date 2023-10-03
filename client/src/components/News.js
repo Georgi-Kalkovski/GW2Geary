fd3af085c076c5183211d1f75afb40a796c8fc5b
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-10-03 12:03:24 +0300

    uploading a fix for meta description.

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -8,7 +8,7 @@ function News() {
             <Helmet>
                 <title>GW2Geary - News</title>
                 <meta
-                    name="og:description"
+                    name="description"
                     content="All the News about GW2Geary are here! 
                              Latest news: Added Relic and Power Core to the character's preview."
                 />
