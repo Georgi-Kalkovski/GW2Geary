77bfa123c20d95f637d6d76882f8e70952438943
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-10-18 09:36:25 +0300

    Added news svg.

--- client/src/components/Search/SearchNews.js
+++ client/src/components/Search/SearchNews.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import NewsList from '../NewsList';
-
+import NewsSvg from './news.svg'
 function SearchNews() {
   return (
     <div className='flex center'>
       <div className='home-empty-search-box'>
-        <div className='home-welcome' style={{ fontSize: '25px' }}>
-          Latest News
+        <div className='home-welcome flex news-svg' style={{ fontSize: '25px', justifyContent: 'space-between' }}>
+          <img className='' style={{ marginLeft: '-3px', marginTop: '-20px', width: '35px', position: 'relative', left: '0' }} src={NewsSvg} alt="" />
+          <div className='flex center'>Latest News</div>
+          <div style={{ width: '35px' }}></div>
         </div>
         <div className='search-news'>
-          <NewsList isSearchNews limit={6}/>
+          <NewsList isSearchNews limit={6} />
         </div>
       </div>
     </div>
