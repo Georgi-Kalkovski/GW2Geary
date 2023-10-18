8af73e0814b76f182c44b6b787889e23cf5992e2
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-10-18 08:36:31 +0300

    Update SearchWelcome.js

--- client/src/components/Search/SearchWelcome.js
+++ client/src/components/Search/SearchWelcome.js
@@ -10,17 +10,20 @@ function SearchWelcome() {
                     Welcome to <span className="gw2-logo-style">GW2</span>
                     <span className="geary-logo-style">Geary</span>!
                 </Row>
-                <Row>A place where you can inspect or share equipment and builds of registered accounts and their characters.
-                    If someone adds a valid{' '}
-                    <Link to="https://account.arena.net/applications" style={{ color: '#d70000' }} target="_blank">
-                        GW2 API key
-                    </Link>
-                    <span style={{ fontSize: '15px' }}>(<span className='yellow-highlight'></span>
-                        <span style={{ borderBottom: '1px dotted' }}>account</span>
-                        , <span style={{ borderBottom: '1px dotted' }}>characters</span>
-                        , <span style={{ borderBottom: '1px dotted' }}>builds</span>
-                        , <span style={{ borderBottom: '1px dotted' }}>progression</span>) </span>
-                    with us and grants access, you'll be able to find and inspect them.
+                <Row className='search-news'>
+                    <Row>A place where you can inspect or share equipment and builds of registered accounts and their characters.
+                        If someone adds a valid{' '}
+                        <Link to="https://account.arena.net/applications" style={{ color: '#d70000' }} target="_blank">
+                            GW2 API key
+                        </Link> with us and grants access, you'll be able to find and inspect them.
+                        Make sure to add the required API permissions 
+                        <span style={{ fontSize: '15px' }}> (<span className='yellow-highlight'></span>
+                            <span className='yellow-highlight' style={{ borderBottom: '1px dotted' }}>account</span>
+                            , <span className='yellow-highlight' style={{ borderBottom: '1px dotted' }}>characters</span>
+                            , <span className='yellow-highlight' style={{ borderBottom: '1px dotted' }}>builds</span>
+                            , <span className='yellow-highlight' style={{ borderBottom: '1px dotted' }}>progression</span>).
+                        </span>
+                    </Row>
                 </Row>
             </Col>
         </Container>);
