107d48252e63d65cf33d582faab5c6ca9f02c315
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2024-06-17 09:04:31 +0300

    Update SendMail.js

--- client/src/components/Login/SendMail.js
+++ client/src/components/Login/SendMail.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useState, useCallback } from "react";
-import axios from "axios";
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
 
 function SendMail({ AuthService }) {
   const ip = "https://gw2geary.com/api";
-  // const [usernameInput, setUsernameInput] = useState("");
   const [emailInput, setEmailInput] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleCheckUsername = async () => {
-    const userFound = (await AuthService.getEmail({ email: emailInput })).data.user.find(
-      (u) => u.email === emailInput
-    );
-    axios
-      .post(`${ip}/reset-password`, { user: userFound }, { headers: { "Content-Type": "application/json" } })
-      .then((response) => {
-        console.log(response.data);
+    try {
+      const response = await AuthService.getEmail(emailInput);
+      const userFound = response.data.user;
+
+      if (userFound.length > 0) {
+        await axios.post(`${ip}/reset-password`, { user: userFound[0] }, { headers: { "Content-Type": "application/json" } });
         setErrorMessage("Email sent successfully!");
-      })
-      .catch((error) => {
-        console.error("Error checking username:", error);
-      });
+      } else {
+        setErrorMessage("User not found.");
+      }
+    } catch (error) {
+      console.error("Error checking username:", error);
+      setErrorMessage("An error occurred. Please try again.");
+    }
   };
 
   const handleKeyPress = useCallback(
@@ -33,50 +33,37 @@ function SendMail({ AuthService }) {
   );
 
   return (
-    <>
-      {/* New input and button for checking the username */}
-      <div className="form-group">
-        <h2 style={{ textAlign: "center", marginBlockEnd: 0 }}>
-          Reset password
-        </h2>
-        <p style={{ textAlign: "center" }}>
-          <span>Enter your Email and we </span>{" "}
-          <span> will send you</span>
-          <span> reset password email.</span>
-        </p>
-        <div className="flex column center">
-          {/* <label htmlFor="check-username">Username</label>
-          <input
-            type="text"
-            className="form-control"
-            name="check-username"
-            value={usernameInput}
-            onChange={(e) => setUsernameInput(e.target.value)}
-            onKeyDown={handleKeyPress}
-          /> */}
-
-          <label htmlFor="check-email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            name="check-email"
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-            onKeyDown={handleKeyPress}
-          />
-          {errorMessage && <p style={{ textAlign: "center" }}>{errorMessage}</p>}
-          <div className="flex center">
-            <button
-              className="basic-button"
-              type="button"
-              onClick={handleCheckUsername}
-            >
-              Send an Email
-            </button>
-          </div>
+    <div className="form-group">
+      <h2 style={{ textAlign: "center", marginBlockEnd: 0 }}>
+        Reset password
+      </h2>
+      <p style={{ textAlign: "center" }}>
+        <span>Enter your Email and we </span>{" "}
+        <span> will send you</span>
+        <span> reset password email.</span>
+      </p>
+      <div className="flex column center">
+        <label htmlFor="check-email">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          name="check-email"
+          value={emailInput}
+          onChange={(e) => setEmailInput(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+        {errorMessage && <p style={{ textAlign: "center" }}>{errorMessage}</p>}
+        <div className="flex center">
+          <button
+            className="basic-button"
+            type="button"
+            onClick={handleCheckUsername}
+          >
+            Send an Email
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
